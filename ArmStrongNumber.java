131c987282fe4de4033e8a485aa8d31b85373792
Nag46214 <97864157+Nag46214@users.noreply.github.com>
2022-07-27 11:07:27 +0530

    Create ArmStrongNumber.java

---
+++ ArmStrongNumber.java
@@ -0,0 +1,42 @@
+// Armstrong_Number by Nagarjuna
+import java.util.*;
+public class Main
+{
+  public static void main (String[]args)
+  {
+    Scanner sc = new Scanner (System.in);
+      System.out.println ("Enter the Number");
+    int number = sc.nextInt ();
+    if (calculatePowerAndSum(number) == number)
+      System.out.println (number + " is Armstrong_Number");
+    else
+        System.out.println (number + " is not an Armstrong_Number");
+  }
+  // Method to find the length of the Given Number
+  static int getLength (int len)
+  {
+    int length = 0;
+    while (len != 0)
+      {
+	len /= 10;
+	length++;
+      }
+    return length;
+  }
+  //Method to find the Power and Equivalent sum of the each digits w.r.to Power
+  static int calculatePowerAndSum(int num)
+  {
+    int rem = 0, res = 0;
+    int temp = num;
+    int sum = 0;
+    while (temp > 0)
+      {
+	rem = temp % 10;
+	res = (int) Math.pow (rem, getLength (num));
+  //Here getLength Method is called  inside this method (power and sum calculation) to find the digits final value in its power.
+	sum += res;
+	temp /= 10;
+      }
+    return sum;
+  }
+}
